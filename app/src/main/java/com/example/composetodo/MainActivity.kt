d6e94d76840358a5e0821a1bcc484bcb00202f1c
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-07-09 16:46:00 -0300

    Dependencies

--- app/src/main/java/com/example/composetodo/MainActivity.kt
+++ app/src/main/java/com/example/composetodo/MainActivity.kt
@@ -17,27 +17,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTodoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTodoTheme {
-        Greeting("Android")
-    }
-}
