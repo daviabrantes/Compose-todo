d36704dad72b95f00755ccc68141f8b058469fb3
Davi e Tal <davi.souza.abrantes@gmail.com>
2022-08-04 21:40:18 -0300

    Display Dynamic Content

--- app/src/main/java/com/example/composetodo/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/composetodo/ui/screens/task/TaskScreen.kt
@@ -3,16 +3,24 @@ package com.example.composetodo.ui.screens.task
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import com.example.composetodo.data.models.Priority
 import com.example.composetodo.data.models.ToDoTask
+import com.example.composetodo.ui.viewmodels.SharedViewModel
 import com.example.composetodo.util.Action
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskScreen(
     selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
+
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
     Scaffold(
         topBar = {
             TaskAppBar(
@@ -22,12 +30,12 @@ fun TaskScreen(
         },
         content = {
             TaskContent(
-                title = "",
-                onTitleChange = { },
-                description = "",
-                onDescriptionChange = {},
-                priority = Priority.LOW,
-                onPrioritySelected = {}
+                title = title,
+                onTitleChange = { sharedViewModel.title.value = it },
+                description = description,
+                onDescriptionChange = { sharedViewModel.description.value = it },
+                priority = priority,
+                onPrioritySelected = { sharedViewModel.priority.value = it }
             )
         }
     )
